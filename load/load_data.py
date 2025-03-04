2a51510d663dd0a13c5b85ed288dcac3f202465c
Enzo Castro <nzcstr@Enzos-MacBook-Air.local>
2025-03-04 11:51:45 +0100

    Simplified code in 'load.py'. New functions stored in 'misc.py' for downloading, extracting and exporting data

--- load/load_data.py
+++ load/load_data.py
@@ -1,54 +1,27 @@
 from pymongo import MongoClient
 import pandas as pd
 pd.set_option("display.max_columns", 6)
-import wget
-import os
-from zipfile import ZipFile
+from nzcstr_tools.misc import download_file_url, extract_zip, csv_to_mongo
+import os, glob
 
 
 def main():
 # Download data
     DATA_DIR="./data"
     URL = "https://www.kaggle.com/api/v1/datasets/download/shivamb/netflix-shows"
-    FILE_NAME = "netflix-shows.zip"
+    FILE_NAME = "netflix-shows.csv"
     DB_NAME = "netflix_db"
     COLLECTION_NAME = "shows"
-    if not os.path.isdir(DATA_DIR):
-        os.mkdir(DATA_DIR)
+    MONGO_URI = "mongodb://localhost:27017"
 
-    dst = os.path.join(DATA_DIR, FILE_NAME)
-    if not os.path.isfile(dst):
-        wget.download(URL, dst)
-        print("Data downloaded")
-    else:
-        print("Data already downloaded")
+    path_to_file, file_name = download_file_url(DATA_DIR, URL)
 
     #Extract
-    if not os.path.isdir(os.path.join(DATA_DIR, FILE_NAME[:-4])):
-        with ZipFile(dst, 'r') as zip_file:
-            zip_file.extractall(DATA_DIR)
-        print(f"Data extracted into {DATA_DIR}")
-    else:
-        print("Data already extracted")
+    extracted_dir = extract_zip(path_to_file, DATA_DIR)
 
-    # Connect to MongoDB running in Docker
-    client = MongoClient("mongodb://localhost:27017/")
-
-    # Select db and collection
-    db = client[DB_NAME]
-    collection = db[COLLECTION_NAME]
-
-    src_csv = os.path.join(DATA_DIR, FILE_NAME)
-    df = pd.read_csv(src_csv)
-
-    # Convert dataframe to a list of dictionaries.
-    # Each entry is a single dictionary.
-    data = df.to_dict(orient="records")
-
-    # Insert data into MongoDB
-    collection.insert_many(data)
-
-    print("Data has been loaded into MongoDB!")
+    #Export to MongoDB
+    path_to_csv = glob.glob(os.path.join(extracted_dir, "*.csv"))[0] # There should be only one CSV file
+    csv_to_mongo(path_to_csv, MONGO_URI, DB_NAME, COLLECTION_NAME)
 
 if __name__ == "__main__":
     main()
---
+++ load/nzcstr_tools/misc.py
@@ -0,0 +1,46 @@
+import os
+import wget
+from zipfile import ZipFile
+from pymongo import MongoClient
+import pandas as pd
+
+def download_file_url(dst_dir:str, url:str)-> tuple[str, str]:
+
+    file_name = url.split("/")[-1] + ".zip"
+    dst_file_path = os.path.join(dst_dir, file_name)
+
+    if not os.path.isdir(dst_dir):
+        os.mkdir(dst_dir)
+
+    if not os.path.isfile(dst_file_path):
+        wget.download(url, dst_file_path)
+        print("Data downloaded")
+    else:
+        print("Data already downloaded")
+
+    return dst_file_path, file_name
+
+def extract_zip(zip_file_path:str, dst_dir:str) -> str:
+    dst_folder_name = zip_file_path.split("/")[-1].split(".")[0]
+    extracting_dir = os.path.join(dst_dir, dst_folder_name)
+    if not os.path.isdir(extracting_dir):
+        with ZipFile(zip_file_path, 'r') as zip_file:
+            zip_file.extractall(extracting_dir)
+            print("Data extracted")
+    else:
+        print("Data already extracted")
+    return extracting_dir
+def csv_to_mongo(csv_file_path:str, mongo_uri:str, mongo_db:str, mongo_collection:str) -> None:
+
+    # Get MongoDB connection details
+    client = MongoClient(mongo_uri)
+    db = client[mongo_db]
+    collection = db[mongo_collection]
+
+    # Read data and format
+    df = pd.read_csv(csv_file_path)
+    data = df.to_dict(orient='records')
+
+    # Export to mongo
+    collection.insert_many(data)
+    print("Data has been loaded into MongoDB!")
